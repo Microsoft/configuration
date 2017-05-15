7e770156b9521ed5c68a28d0337b513c54579a97
manikarthik-k <manikarthik9661@gmail.com>
2017-05-14 17:01:03 -0700

    Create matplotlib.sh

---
+++ matplotlib.sh
@@ -0,0 +1,7 @@
+sudo rm /edx/app/edxapp/edx-platform/requirements/edx-sandbox/base.txt
+
+sudo cp /tmp/configuration/base.txt  /edx/app/edxapp/edx-platform/requirements/edx-sandbox/base.txt
+
+sudo rm /edx/app/edxapp/edx-platform/requirements/edx-sandbox/post.txt
+
+sudo cp /tmp/configuration/post.txt  /edx/app/edxapp/edx-platform/requirements/edx-sandbox/post.txt
