550df32a453f2686fe781048e05dffdd38568cad
localstepdo <stepdo@microsoft.com>
2018-01-22 09:33:32 -0800

    try https for pypi

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -122,7 +122,7 @@ apt-get install -y python2.7 python2.7-dev python-pip python-apt python-yaml pyt
 # Workaround. Upgrade to latest and then
 # potentially downgrade to preferred version.
 #apt-get install -y python2.7 python2.7-dev python-pip python-apt python-yaml python-jinja2
-pip install --upgrade pip
+pip install --upgrade pip --index-url=https://pypi.python.org/simple/
 pip install --upgrade pip=="${PIP_VERSION}"
 #apt-get install -y build-essential sudo git-core libmysqlclient-dev
 
